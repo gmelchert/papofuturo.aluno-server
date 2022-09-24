1171d6051a4d426156905ef42d7a563ea4570029
Gabriel melchert <gfmelchert@gmail.com>
2022-09-24 14:03:17 -0300

    rota para cpf corrigida

--- routes/cpf.js
+++ routes/cpf.js
@@ -17,14 +17,52 @@ const router = express.Router()
 
 router.get('/:cpf', async (req, res) => {
     const
-        cpf = req.params.cpf,
+        cpf = req.params.cpf.replaceAll('.', '').replace('-', ''),
         table = 'cadastro',
-        specificfiltersObjects = { cpf },
+        specificfiltersObjects = { cpf, admin: 0 },
         filter = { req, table, specificfiltersObjects }
 
     utilsDb.get({
         table,
         filter
+    }).then(result => ok(res, result))
+    .catch(err => error(res, fail.query, err))
+})
+
+router.post('', async (req, res) => {
+    const {
+        cpf,
+        rg,
+        fone,
+        email,
+        idade,
+        sobrenome,
+        nome,
+        confirmarSenha,
+        senha
+    } = req.body
+
+    if (confirmarSenha !== senha) return error(res, 'As senhas estão diferentes.')
+
+    if (Object.values(req.body).some(val => !val)) return error(res, 'Todos os campos são obrigatórios.')
+    const
+        table = 'cadastro',
+        values = {
+            cpf: cpf.replaceAll('.', '').replace('-', ''),
+            rg,
+            fone,
+            email,
+            idade,
+            sobrenome,
+            nome,
+            senha
+        },
+        md5Values = ['senha']
+
+    utilsDb.post({
+        table,
+        values,
+        md5Values
     }).then(result => ok(res, result))
     .catch(err => error(res, fail.query, err))
 })
